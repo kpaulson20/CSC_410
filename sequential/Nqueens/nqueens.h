90beb01bb34ac6d01bf04d24d27b305cc7379ec1
Kynedi Paulson <kynedi.paulson@trojans.dsu.edu>
2024-09-15 09:12:39 -0600

    Assignment 1

---
+++ sequential/Nqueens/nqueens.h
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+bool isSafe(int board[], int row, int col, int n) {
+    for (int i = 0; i < col; i++) {
+        if (board[i] == row || abs(board[i] - row) == abs(i - col)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+void solveNQueensUtil(int board[], int col, int n) {
+    // Base case
+        // write your code here
+
+    // Recursive case
+        // write your code here
+}
