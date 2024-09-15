c23b207db672ae68105ced7678686207492db987
Kynedi Paulson <kynedi.paulson@trojans.dsu.edu>
2024-09-15 10:28:07 -0600

    Assignment 1

--- sequential/matrixMul/matrixMul.h
+++ sequential/matrixMul/matrixMul.h
@@ -16,7 +16,13 @@ void matrixMultiply(int A[N][N], int B[N][N], int C[N][N]) {
     // Write your code here
     for (int i = 0; i < N; i++)
     {
-        printf("%d", C[N][N]);
+        for (int j = 0; j < N; j++)
+        {
+            C[i][j] = 0;
+            for (int k = 0; k < N; k++)
+            {
+                C[i][j] += A[i][j] * B[i][j];
+            }
+        }  
     }
-    printf ("\n");
 }
