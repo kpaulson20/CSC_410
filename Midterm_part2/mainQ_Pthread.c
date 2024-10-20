680a23b91b9796f91c37b672db81f2980b8d457f
Kynedi Paulson <kynedi.paulson@trojans.dsu.edu>
2024-10-20 09:31:14 -0600

    Midterm_part2

--- Midterm_part2/mainQ_Pthread.c
+++ Midterm_part2/mainQ_Pthread.c
@@ -1,31 +1,20 @@
 #include "nqueens_Pthread.h"
 
 int main() {
-    int n = 8;
-    int *board = (int*)malloc(n * sizeof(int));
-    pthread_mutex_t mutex;
-
+    int n = 8; // Size of the board
+    int* board = (int*)malloc(n * sizeof(int));
     if (board == NULL) {
         fprintf(stderr, "Memory allocation failed\n");
         return 1;
     }
 
-    pthread_mutex_init(&mutex, NULL);
-
     for (int i = 0; i < n; i++) {
-        board[i] = -1;
+        board[i] = -1; // Initialize the board
     }
 
-    for (int i = 0; i < n; i++) {
-        board[0] = i;
+    ThreadData_t data = {board, 0, n};
+    threadSolveNQueens((void*)&data);
 
-        ThreadData data = { .board = board, .col = 0, .n = n, .mutex = &mutex };
-        pthread_t thread;
-        pthread_create(&thread, NULL, solveNQueensUtil, &data);
-        pthread_detach(thread);
-    }
-
-    pthread_mutex_destroy(&mutex);
     free(board);
     return 0;
 }
--- Midterm_part2/nqueens_Pthread.h
+++ Midterm_part2/nqueens_Pthread.h
@@ -1,3 +1,6 @@
+#ifndef NQUEENS_H
+#define NQUEENS_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -8,8 +11,12 @@ typedef struct {
     int *board;
     int col;
     int n;
-    pthread_mutex_t *mutex;
-} ThreadData;
+} ThreadData_t;
+
+bool isSafe(int board[], int row, int col, int n);
+void* threadSolveNQueens(void* arg);
+void solveNQueensUtil(int board[], int col, int n);
+void printBoard(int board[], int n);
 
 bool isSafe(int board[], int row, int col, int n) {
     for (int i = 0; i < col; i++) {
@@ -19,40 +26,52 @@ bool isSafe(int board[], int row, int col, int n) {
     }
     return true;
 }
-void *solveNQueensUtil(void *arg) {
-    ThreadData *data = (ThreadData *)arg;
-    int *board = data->board;
+
+void* threadSolveNQueens(void* arg) {
+    ThreadData_t *data = (ThreadData_t*)arg;
+    int* board = data->board;
     int col = data->col;
     int n = data->n;
-    pthread_mutex_t *mutex = data->mutex;
 
+    // Call the solveNQueensUtil function
+    solveNQueensUtil(board, col, n);
+    
+    return NULL;
+}
+
+void solveNQueensUtil(int board[], int col, int n) {
+    // Base case
+    if (col >= n) {
+        printBoard(board, n);
+        return;
+    }
+
+    // Recursive case
     for (int i = 0; i < n; i++) {
         if (isSafe(board, i, col, n)) {
             board[col] = i;
 
-            pthread_mutex_lock(mutex);
+            // Create a new thread for the next column
+            ThreadData_t *newData = malloc(sizeof(ThreadData_t));
+            newData->board = board;
+            newData->col = col + 1;
+            newData->n = n;
 
-            if (col + 1 >= n){
-                for (int j = 0; j < n; j++){
-                    printf("%d", board[j]);
-                }
-                printf("\n");
-                pthread_mutex_unlock(mutex);
-            } else {
-                ThreadData new_data = { .board = board, .col = col + 1, .n = n, .mutex = mutex };
-                pthread_t thread;
-                pthread_create(&thread, NULL, solveNQueensUtil, &new_data);
-                pthread_join(thread, NULL);
-            }
+            pthread_t thread;
+            pthread_create(&thread, NULL, threadSolveNQueens, (void*)newData);
+            pthread_detach(thread); // Detach the thread so we don't need to join
+
+            // For synchronization, wait for the current thread to complete
+            pthread_join(thread, NULL);
         }
     }
-    return NULL;
-    // Recursive case
-    // write your code here
-    //for (int i = 0; i < n; i++){
-    //    if (isSafe(board, i, col, n)){
-    //        board[col] = i;
-    //        solveNQueensUtil(board, col + 1, n);
-    //    }
-    //}
 }
+
+void printBoard(int board[], int n) {
+    for (int i = 0; i < n; i++) {
+            printf("%d ", board[i]);
+        }
+        printf("\n");
+}
+
+#endif // NQUEENS_H
